2f4b708ba736e6bedd85b996ea5a2e2fecccbb5f
Cliff Sparks <cliff.sparks@uclmail.net>
2022-09-11 22:11:38 +0100

    New Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaProject
+QA JavaProject
---
+++ src/main/java/com/qa/project/JavaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.qa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+	}
+}
