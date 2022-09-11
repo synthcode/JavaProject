a5adeecda44f02f20ae6672a7c6e3ebb5c204de1
Cliff Sparks <cliff.sparks@uclmail.net>
2022-09-11 22:39:38 +0100

    Hello World!

--- src/main/java/com/qa/project/JavaProjectApplication.java
+++ src/main/java/com/qa/project/JavaProjectApplication.java
@@ -8,5 +8,6 @@ public class JavaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaProjectApplication.class, args);
+		System.out.println("Hello World!");
 	}
 }
